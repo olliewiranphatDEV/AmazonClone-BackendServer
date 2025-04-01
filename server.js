d6a23ff75214dd928bd478423544b2759f4393a1
olliewiranphatDEV <olliewiranphat@gmail.com>
2025-04-01 16:45:48 +0700

    CORS

--- server.js
+++ server.js
@@ -12,12 +12,25 @@ const sellerRouter = require('./router/sellerRouter')
 const adminRouter = require('./router/adminRouter')
 const categoryRouter = require('./router/categoryRouter')
 const { clerkMiddleware } = require('@clerk/express')
+const fs = require('fs')
+
+if (process.env.CA_PEM_B64) {
+    fs.writeFileSync(
+        './tmp/ca.pem',
+        Buffer.from(process.env.CA_PEM_B64, 'base64').toString('utf-8')
+    )
+}
 
 
 ///// Middlewares :
 app.use(clerkMiddleware()) //req.auth
 ///// Connect Frontend - Backend :
-app.use(cors())
+///// Connect Frontend - Backend :
+app.use(cors({
+    origin: 'https://amazon-clone-frontend-web.vercel.app',
+    credentials: true
+}))
+
 ///// Read JSON req.body from Frontend :
 app.use(express.json({ limit: "10mb" })) //Max Payload size Server can receive
 app.use(morgan('dev'))
